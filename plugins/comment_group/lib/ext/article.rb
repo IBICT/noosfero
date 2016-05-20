56ce87a7f7418c086f2d6535faf905cd191886a7
Leandro Nunes dos Santos <leandronunes@gmail.com>
2016-05-20 18:41:19 -0300

    fix comments bug

--- plugins/comment_group/lib/ext/article.rb
+++ plugins/comment_group/lib/ext/article.rb
@@ -10,7 +10,7 @@ class Article
 
   def not_empty_group_comments_removed
     if body && body_changed?
-      groups_with_comments = Comment.select('DISTINCT group_id').where(source_id: self.id).map(&:group_id).compact
+      groups_with_comments = Comment.select('DISTINCT comments.group_id').where(source_id: self.id).map(&:group_id).compact
       groups = Nokogiri::HTML.fragment(body.to_s).css('.macro').collect{|element| element['data-macro-group_id'].to_i}
       errors[:base] << (N_('Not empty group comment cannot be removed')) unless (groups_with_comments-groups).empty?
     end
