ed75a4919952832c476317f87ccbe156bde19ec6
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-05-03 09:28:40 -0300

    fix merge conmflit

--- app/controllers/admin/environment_design_controller.rb
+++ app/controllers/admin/environment_design_controller.rb
@@ -2,18 +2,8 @@ class EnvironmentDesignController < BoxOrganizerController
 
   protect 'edit_environment_design', :environment
 
-  def filtered_available_blocks(blocks=nil)
-    filtered_available_blocks = []
-    blocks.each { |block| filtered_available_blocks << block unless @environment.disabled_blocks.include?(block.name) }
-    filtered_available_blocks
-  end
-
   def available_blocks
     boxes_holder.available_blocks(user) + plugins.dispatch(:extra_blocks, :type => Environment)
-  end
-
-  def index
-    available_blocks
   end
 
 end
